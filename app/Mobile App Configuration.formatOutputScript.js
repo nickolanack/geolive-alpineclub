f47993ce1ec29a5a689223cf6ec3057f8cd3d39a
nickolanack <nickblackwell82@gmail.com>
2018-01-25 13:07:15 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Mobile App Configuration.formatOutputScript.js
+++ app/Mobile App Configuration.formatOutputScript.js
@@ -1,2 +1,5 @@
-$parameters['client']=GetClient()->getUserMetadata();
+$parameters['client']=($client=GetClient()->getUserMetadata());
+if($client['id']>0){
+    $parameters['client']['attributes']=(new attributes\Record('deviceUserAttributes'))->getValues($client['id'],'user');
+}
 return $parameters;
