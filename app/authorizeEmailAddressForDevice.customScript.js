e7260fa108ff252d18238ada6b011d7fddf916ea
nickolanack <nickblackwell82@gmail.com>
2018-01-25 16:01:06 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/authorizeEmailAddressForDevice.customScript.js
+++ app/authorizeEmailAddressForDevice.customScript.js
@@ -1,14 +1,9 @@
 <?php
 GetPlugin('Attributes');
 
-Emit('authorizeEmailStart',$eventArgs);
-
 (new attributes\Record('deviceUserAttributes'))
     ->setValues($eventArgs->user, 'user', array(
         'authorizedEmail'=>$eventArgs->email
     ));
-    
-    
-Emit('authorizeEmailEnd',$eventArgs);
 
 ?>
