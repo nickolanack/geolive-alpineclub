a3bfbc22f718a5557f7d838c642f55359a80c0cd
nickolanack <nickblackwell82@gmail.com>
2018-01-27 08:30:38 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/authorizeEmailAddressForDevice.customScript.js
+++ app/authorizeEmailAddressForDevice.customScript.js
@@ -1,13 +1,22 @@
 <?php
 
 //This event was triggered from an event token bound to an action link passed to a user.
+//create event token
+//$token=($links=GetPlugin('Links'))->createLinkEventCode('onAuthorizeEmailAddressForDevice', array(
+//    'user'=>$clientId,
+//    'email'=>$json->email
+//));
 
 
 GetPlugin('Attributes');
 
 (new attributes\Record('deviceUserAttributes'))
     ->setValues($eventArgs->user, 'user', array(
-        'authorizedEmail'=>$eventArgs->email
+        'authorizedEmail'=>$eventArgs->email,
+        'authEmailStatus'=>'approved'
     ));
+
+//user should now have matching attributes: authorizedEmail=authEmail
+
 
 ?>
