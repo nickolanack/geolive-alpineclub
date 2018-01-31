14aeb03ae37fb049462d344cc5f6290828269ff9
nickolanack <nickblackwell82@gmail.com>
2018-01-30 21:47:57 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/listMarkers.customScript.js
+++ app/listMarkers.customScript.js
@@ -3,4 +3,8 @@ GetPlugin('Maps');
 
 $layer=(int)$json->layer;
 
-return array("items"=>(new spatial\Features())->listLayerItems($layer)->withUserAccessFilter()->addUserAttributes('deviceUserAttributes')->get());
+return array(
+    "items"=>(new spatial\Features())->listLayerItems($layer)
+        ->addUserAttributes('deviceUserAttributes')
+        ->get()
+    );
