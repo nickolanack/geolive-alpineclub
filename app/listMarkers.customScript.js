c71df2e8612d5b76e0676dae6bdf38ca94138e18
nickolanack <nickblackwell82@gmail.com>
2018-02-05 09:07:53 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/listMarkers.customScript.js
+++ app/listMarkers.customScript.js
@@ -2,14 +2,15 @@ GetPlugin('Maps');
 
 
 $layer=(int)$json->layer;
+$defaultProfileIcon=GetWidget("mobile-app-config")->getParameter("profile-image");
 
 return array(
     "items"=>(new spatial\Features())->listLayerItems($layer)
         ->appendUserAttributes('deviceUserAttributes')
-        ->map(function($item){
+        ->map(function($item)use($defaultProfileIcon){
             
             $profileIcon=$item['user-attributes']['deviceUserAttributes']['profile'];
-            $item['user-attributes']['deviceUserAttributes']['profile']=GetWidget("")->getParameter("")
+            $item['user-attributes']['deviceUserAttributes']['profile']=$defaultProfileIcon;
             
             return $item;
             
