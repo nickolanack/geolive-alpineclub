09fe94e5fc29d76c87f4e58550a13b9c9fb1dc7d
nickolanack <nickblackwell82@gmail.com>
2018-04-03 10:45:24 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/featuredItems.content.1.getItemListScript.js
@@ -0,0 +1,38 @@
+
+var itemlist=<?php
+GetPlugin('Maps');
+
+
+$layer=1;
+$defaultProfileIcon=GetWidget("mobile-app-config")->getParameter("profile-image")[0];
+
+echo json_encode(array_slice((new spatial\Features())->listLayerItems($layer)
+        ->appendUserInfo()
+        ->appendUserAttributes('deviceUserAttributes')
+        ->map(function($item)use($defaultProfileIcon){
+            
+            $profileIcon=$item['user-attributes']['deviceUserAttributes']['profileImage'];
+            $item['user-attributes']['deviceUserAttributes']['isDefaultProfileImage']=false;
+            if(empty($profileIcon)||(!HtmlDocument()->isLocalFileUrl($profileIcon))){
+                $item['user-attributes']['deviceUserAttributes']['profileImage']=$defaultProfileIcon;
+                $item['user-attributes']['deviceUserAttributes']['isDefaultProfileImage']=true;
+            }
+            
+            
+            
+            
+            
+            
+            return $item;
+            
+        }), 0, 5)
+    );
+
+
+?>
+
+return itemlist.map(function(item){
+    
+    return new MockDataTypeItem(item);
+    
+});
