a5a9bcae213de29fce91aeb2057501203cc69fe8
nickolanack <nickblackwell82@gmail.com>
2018-04-05 13:54:58 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Link Account.customScript.js
+++ app/Link Account.customScript.js
@@ -1,6 +1,9 @@
 <?php
-$linkEmailAddress='nickblackwell82@gmail.com';
 
-echo json_encode(Core::GetUsersource()->emailExists($linkEmailAddress));
+$linkEmailAddress='nickblackwell82@gmail.com';
+$users=Core::GetUsersource()->emailExists($linkEmailAddress);
+if(!empty($users))
+
+echo 'Should Link Account! '.json_encode($users);
 
 ?>
