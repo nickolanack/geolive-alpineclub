2224669d85d9bbcc1de5577d7df0133db39ccb5d
nickolanack <nickblackwell82@gmail.com>
2018-04-05 15:09:38 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Link Account.customScript.js
+++ app/Link Account.customScript.js
@@ -1,11 +1,17 @@
-<?php
-
 $linkEmailAddress='nickblackwell82@gmail.com';
 $users=Core::GetUsersource()->emailExists($linkEmailAddress);
 if(!empty($users)){
     echo 'Should Link Account! '.json_encode($users);
+}else{
+    echo 'Nothing to link';
 }
 
+GetPlugin('Attributes');
+
+    echo (new attributes\Filter())->toQuery('{"join":"intersect","table":"deviceUserAttributes","set":"*","filters":[
+                        {"field":"authorizedEmail","value":"' . $linkEmailAddress . '"}
+                        
+                    ]}');
 
 
 ?>
