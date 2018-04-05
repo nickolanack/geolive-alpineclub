259544f06a42eaab2c9884076bae0f5f4f60e9f3
nickolanack <nickblackwell82@gmail.com>
2018-04-05 14:43:23 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/Link Account.customScript.js
+++ app/Link Account.customScript.js
@@ -4,6 +4,8 @@ $linkEmailAddress='nickblackwell82@gmail.com';
 $users=Core::GetUsersource()->emailExists($linkEmailAddress);
 if(!empty($users)){
     echo 'Should Link Account! '.json_encode($users);
+}else{
+    echo 'Nothing to link';
 }
 
 
