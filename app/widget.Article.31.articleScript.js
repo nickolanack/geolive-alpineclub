b5c44d88a3ea7e14f728a03c6300f6e08ed5af47
nickolanack <nickblackwell82@gmail.com>
2018-04-05 21:15:02 -0700

    -- Auto Commit - Changes made through Frontend Interface

---
+++ app/widget.Article.31.articleScript.js
@@ -0,0 +1,10 @@
+window.addEvent("load",function(){
+            
+            setTimeout(function(){
+                
+                window.location.href="";
+                
+            }, 5000)
+            
+            
+        });
