a21573e6b1143744a614a669b38b6886ccf9cf48
nickolanack <nickblackwell82@gmail.com>
2018-06-21 10:49:06 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Alpine Auth Function.customScript.js
+++ app/Alpine Auth Function.customScript.js
@@ -1,5 +1,11 @@
 <?php
 
-Emit('onCheckAlpineAuth', array('args'=>func_get_args()));
+
+
+
+Emit('onAttemptAlpineAuth', array('args'=>func_get_args()));
+
+
+echo "Args: ".json_encode(func_get_args());
 
 ?>
