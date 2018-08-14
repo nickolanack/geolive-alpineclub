152390e531722f5637307ecb53139945cf7c5c4a
nickolanack <nickblackwell82@gmail.com>
2018-08-13 21:52:00 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Alpine Auth Function.customScript.js
+++ app/Alpine Auth Function.customScript.js
@@ -67,7 +67,7 @@ if(($status=$tokenResponse->getStatusCode())!==200){
        'url'=>$tokenUrl,
        'message'=>'Token Response: '.$status,
        'args'=>func_get_args(),
-       'response'=>$tokenResponse->getBody()
+       'response'=>$tokenResponse->getBody().""
        ));
        
     print_r($error);
