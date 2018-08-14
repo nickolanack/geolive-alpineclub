0e6b03b5a5674f8f57af6caf1b50313fa1f91250
nickolanack <nickblackwell82@gmail.com>
2018-08-13 21:32:12 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Alpine Auth Function.customScript.js
+++ app/Alpine Auth Function.customScript.js
@@ -99,24 +99,28 @@ $client = new \GuzzleHttp\Client(array(
     'cookies'=> $cookies    
 ));
 
-$validationResponse = $client->request('GET', 
-    $validationUrl, 
-    array(
-        'http_errors' => false,
-        'headers'=> $headers=array(
-            'request-verification-token'=>$data->access_token,
-            'accept'     => 'application/json',
-            'origin' => 'https://www.alpineclubofcanada.ca',
-            
-			'accept-encoding'=>'gzip, deflate, br',
-			'accept-language'=>'en-US,en;q=0.9,fr;q=0.8',
-			'cache-control'=>' max-age=0',
+$request = $client->createRequest('GET', $validationUrl);
 
-			'upgrade-insecure-requests'=>'1',
-			'user-agent'=>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-        )
-    )
-);
+$request->addHeader('RequestVerificationToken', $data->access_token);
+$request->addHeader('Content-Type', 'application\json');
+$validationResponse = $client->send($request);   
+//     , 
+//     array(
+//         'http_errors' => false,
+//         'headers'=> $headers=array(
+//             'request-verification-token'=>$data->access_token,
+//             'accept'     => 'application/json',
+//             'origin' => 'https://www.alpineclubofcanada.ca',
+            
+// 			'accept-encoding'=>'gzip, deflate, br',
+// 			'accept-language'=>'en-US,en;q=0.9,fr;q=0.8',
+// 			'cache-control'=>' max-age=0',
+
+// 			'upgrade-insecure-requests'=>'1',
+// 			'user-agent'=>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+//         )
+//     )
+// );
 
 
 echo $validationUrl."\n";
