edf20af9e14e069952d57f7500aea65b4e1c2e58
nickolanack <nickblackwell82@gmail.com>
2018-08-20 09:44:43 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/deleteItem.customScript.js
+++ app/deleteItem.customScript.js
@@ -1,7 +1,1 @@
-$result=(new core\DefaultAjaxTreeController())->executeTask('mapitem_delete',(object)array('mapitemId'=>$json->item));
-
-if(is_array($result)){
-    $result['text']='Successfully Deleted Item';
-}
-
-return $result;
+(new core\DefaultAjaxTreeController())->executeTask('mapitem_delete',(object)array('mapitemId'=>$json->item));
